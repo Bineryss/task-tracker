331af85a106f86a6ea57981467d15bfa0b5c90c6
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2023-07-29 01:54:37 +0200

    added swilane component

---
+++ src/components/ProgressColumn.vue
@@ -0,0 +1,61 @@
+<template>
+    <div id="row" class="bg-base-300 rounded-xl overflow-auto flex flex-col">
+        <div id="row-headline" class="px-4 py-2 flex gap-2 items-center sticky top-0 bg-base-300">
+            <h1 class="font-bold">{{ progressValues.title }}</h1>
+            <div class="bg-primary rounded-full aspect-square w-4 place-self-start">
+                <h4 class="text-center text-xs">{{ progressValues.issues?.length }}</h4>
+            </div>
+            <div class="grow" />
+            <button class="flex justify-center items-center gap-1 w-6">
+                <span v-for="i in 3" class="rounded-full aspect-square h-1 bg-base-100" :key="i" />
+            </button>
+        </div>
+
+        <ul class="grid gap-2 mx-2 my-4 grow content-start">
+            <li v-for="id in progressValues.issues" :key="id">
+                <IssueCard :id="id" />
+            </li>
+        </ul>
+        <button class="w-full py-1 bg-base-300 rounded-xl border border-transparent hover:border-primary sticky bottom-0">
+            + Add Item
+        </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ComputedRef, computed } from "vue";
+import IssueCard from "../components/IssueCard.vue"
+
+const props = defineProps<{
+    id: string
+}>()
+
+const progressValues: ComputedRef<List> = computed(() => {
+
+    return tmpDB.find((e) => e.id === props.id)
+})
+
+interface List {
+    id: string,
+    title: string,
+    issues: Array<string>
+}
+
+const tmpDB: Array<List> = [
+    {
+        id: 'todo',
+        title: "ToDo",
+        issues: ['#11', '#12', '#12', '#12']
+    },
+    {
+        id: 'in-progress',
+        title: "In Progress",
+        issues: ['#11']
+    },
+    {
+        id: 'done',
+        title: "Done",
+        issues: ['#11', '#12', '#13']
+    }
+]
+</script>
--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,44 +1,28 @@
 <template>
-    <nav class="sticky top-0 h-16 bg-base-300 flex items-center gap-8 px-4">
+    <nav class="sticky top-0 inset-y-0 h-16 bg-base-300 flex items-center gap-4 px-4 z-50 ">
         <div class="aspect-square h-12 bg-primary justify-self-start" />
-        <ul class="grow flex gap-2">
+        <h1 class="font-bold">Sebastian's Planer</h1>
+        <ul class="ml-4 grow flex gap-2">
             <li>
                 <a>Projekte</a>
             </li>
             <li>
                 <a>Boards</a>
             </li>
+            <li>
+                <a>About</a>
+            </li>
         </ul>
         <button class="bg-primary button">New Issue</button>
     </nav>
-    <div class="grid grid-cols-3 gap-4 mt-12 mx-8">
-        <div id="row" v-for="i in 3" :key="i" class="bg-base-300 rounded-xl h-[800px] overflow-auto">
-            <div id="row-headline" class="px-4 py-2 flex gap-2 items-center sticky top-0 bg-base-300">
-                <h1 class="font-bold">Todo</h1>
-                <div class="bg-primary rounded-full aspect-square w-4 place-self-start">
-                    <h4 class="text-center text-xs">5</h4>
-                </div>
-                <div class="grow" />
-                <button class="flex justify-center items-center gap-1 w-6">
-                    <span v-for="i in 3" class="rounded-full aspect-square h-1 bg-base-100" />
-                </button>
-            </div>
-
-            <ul class="grid gap-2 mx-2 my-4">
-                <li v-for="id in issues" :key="id">
-                    <IssueCard :id="id" />
-                </li>
-            </ul>
-            <button
-                class="w-full py-1 bg-base-300 rounded-b-xl border border-transparent hover:border-primary sticky bottom-0">
-                + Add Item
-            </button>
-        </div>
+    <div class="grid grid-cols-[repeat(3,1fr)] gap-4 mx-4 fixed bottom-0 top-20">
+        <ProgressColumn id="todo" />
+        <ProgressColumn id="in-progress" />
+        <ProgressColumn id="done" />
     </div>
 </template>
 
 <script setup lang="ts">
-import IssueCard from "../components/IssueCard.vue"
+import ProgressColumn from '../components/ProgressColumn.vue';
 
-const issues = ['#11', '#12', '#13']
 </script>
